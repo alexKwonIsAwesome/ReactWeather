9f6ba3424cd5cce6df5a9c7e4b74760d1a7026c9
alexKwonIsAwesome <khwalex@gmail.com>
2016-11-03 18:06:06 +0900

    init api fetching component:
    With axios, api fetching component is created and included in
    handleSearch function in Weather component. When the form is submitted,
    it looks for location and fetch right temperature and renders in
    WeatherMessage.

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,20 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=0421ccd51df3891cc2f76806815d52d6&units=metric';
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(function (res) {
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
+    });
+  }
+}
