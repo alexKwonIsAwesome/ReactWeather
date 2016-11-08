eb98320f4eb4cb8f3bca13602663c06be8279bbb
alexKwonIsAwesome <khwalex@gmail.com>
2016-11-08 23:53:21 +0900

    update about page:
    Styled about page and added links to resources.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,10 +3,21 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About Component</h3>
+      <h1 className="text-center">About</h1>
       <p>Welcome to the about page!</p>
       <p>To use the weather app, type your city name in the form then push submit.
       <br/>The temperature will appear under the form.</p>
+      <p>The main purpose of creating this application is to practice using react.</p>
+      <p>
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+          </li>
+          <li>
+            <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+          </li>
+        </ul>
+      </p>
     </div>
   )
 };
