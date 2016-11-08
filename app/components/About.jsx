b2802e2f63a08f656250ba43fbc3130985eb1e4d
alexKwonIsAwesome <khwalex@gmail.com>
2016-11-08 13:14:44 +0900

    Update paragraph in pages

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,6 +3,9 @@ var React = require('react');
 var About = (props) => {
   return (
     <h3>About Component</h3>
+    <p>Welcome to the about page!</p>
+    <p>To use the weather app, type your city name in the form then push submit.
+    <br>The temperature will appear under the form.</p>
   )
 };
 
