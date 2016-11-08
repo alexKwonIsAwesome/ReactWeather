3f7d874eb5a442f16aa8f50cba777628750f3aed
alexKwonIsAwesome <khwalex@gmail.com>
2016-11-08 13:17:55 +0900

    update components:
    All html in jsx are wrapped in <div> tag.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,10 +2,12 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About Component</h3>
-    <p>Welcome to the about page!</p>
-    <p>To use the weather app, type your city name in the form then push submit.
-    <br>The temperature will appear under the form.</p>
+    <div>
+      <h3>About Component</h3>
+      <p>Welcome to the about page!</p>
+      <p>To use the weather app, type your city name in the form then push submit.
+      <br/>The temperature will appear under the form.</p>
+    </div>
   )
 };
 
